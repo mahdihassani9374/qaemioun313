b83a05212314cf140db96e37ac2b06e59b626cc2
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-01-27 10:44:52 +0330

    modify design

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,14 +1,3 @@
 $(function () {
-  var dd = $('.vticker').easyTicker({
-    direction: 'up',
-    interval: 3000,
-    visible: 1,
-    mousePause: 0,
-    controls: {
-      up: '.vticker-next',
-      down: '.vticker-prev',
-      toggle: '.toggle',
-      stopText: 'Stop !!!'
-    }
-  }).data('easyTicker');
-});
+  
+})
